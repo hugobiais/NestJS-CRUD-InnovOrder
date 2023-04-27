0f60b79ee3a0fa7ed29dd4316081797f286b23cf
Hugo Biais <hugobiais75@gmail.com>
2023-04-27 20:47:22 +0100

    create prisma module and imports

--- IMPORTANTCOMMANDS.md
+++ IMPORTANTCOMMANDS.md
@@ -6,3 +6,5 @@ for prisma, once everything is installed, we run `npx prisma init`
 
 Once we've done the configuration, we can run the following commands:
 `npx prisma migrate dev` (only for developpment because it resets the DB)
+
+To access the db, we can run `npx prisma studio`
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
-  login() {}
+  constructor(private prisma: PrismaService) {}
 
-  signup() {}
+  signup() {
+    return { msg: 'I have signed up' };
+  }
+
+  signin() {
+    return { msg: 'I have signed in' };
+  }
 }
