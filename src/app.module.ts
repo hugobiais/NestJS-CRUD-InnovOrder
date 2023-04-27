012d4e60843b8fe29111dd67cb2f38211b70d75b
Hugo Biais <hugobiais75@gmail.com>
2023-04-27 21:52:07 +0100

    create food module for OpenFoodFact route

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { FoodModule } from './food/food.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { PrismaModule } from './prisma/prisma.module';
     AuthModule,
     UserModule,
     PrismaModule,
+    FoodModule,
   ],
 })
 export class AppModule {}
