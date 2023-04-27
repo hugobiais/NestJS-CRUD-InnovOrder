c1bd2829de3a1e211d574378bdc520ac0988c5d6
Hugo Biais <hugobiais75@gmail.com>
2023-04-27 21:10:45 +0100

    modify schema with unique login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup() {
-    return { msg: 'I have signed up' };
+  async signup(dto: AuthDto) {
+    //generate the pass hash
+    const hash = await argon.hash(dto.password);
+
+    // save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        login: dto.login,
+        hash,
+      },
+    });
+
+    // return the user
+    return user;
   }
 
   signin() {
