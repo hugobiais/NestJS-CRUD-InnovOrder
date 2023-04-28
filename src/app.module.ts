ea5b05d9a7ca55a25c01658d8b3f74e93b046cb8
Hugo Biais <hugobiais75@gmail.com>
2023-04-28 03:11:23 +0100

    implement caching system for API calls using NestJS cache-manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { FoodModule } from './food/food.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.register({ isGlobal: true }),
     AuthModule,
     UserModule,
     PrismaModule,
--- src/food/food.controller.ts
+++ src/food/food.controller.ts
@@ -2,32 +2,47 @@ import {
   Controller,
   Get,
   Param,
+  Inject,
   UseGuards,
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
 import { FoodService } from './food.service';
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 @UseGuards(JwtGuard)
 @Controller('food')
 export class FoodController {
-  constructor(private readonly foodService: FoodService) {}
+  constructor(
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private readonly foodService: FoodService,
+  ) {}
 
   @Get(':barcode')
   async getProduct(@Param('barcode') barcode: string) {
-    try {
-      const product = await this.foodService.getProduct(barcode);
-      return product;
-    } catch (error) {
-      if (error.response?.status === 404) {
-        throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
-      } else {
-        throw new HttpException(
-          'Internal server error',
-          HttpStatus.INTERNAL_SERVER_ERROR,
-        );
+    const cachedProduct = await this.cacheManager.get(barcode);
+    if (cachedProduct) {
+      console.log('Cache hit!');
+      return cachedProduct;
+    } else {
+      console.log('Cache miss!');
+      try {
+        const product = await this.foodService.getProduct(barcode);
+        await this.cacheManager.set(barcode, product, 300000); // set the TTL to 5mn
+        return product;
+      } catch (error) {
+        if (error.response?.status === 404) {
+          throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
+        } else {
+          throw new HttpException(
+            'Internal server error',
+            HttpStatus.INTERNAL_SERVER_ERROR,
+          );
+        }
       }
     }
+
   }
 }
