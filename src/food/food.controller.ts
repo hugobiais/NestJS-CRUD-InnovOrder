9d61c26e59a70eb17a32a336ba0105c84d8ebabf
Hugo Biais <hugobiais75@gmail.com>
2023-04-28 01:31:15 +0100

    implement protected openfoodfact route with error handling

--- src/food/food.controller.ts
+++ src/food/food.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  UseGuards,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { FoodService } from './food.service';
 
+@UseGuards(JwtGuard)
 @Controller('food')
-export class FoodController {}
+export class FoodController {
+  constructor(private readonly foodService: FoodService) {}
+
+  @Get(':barcode')
+  async getProduct(@Param('barcode') barcode: string) {
+    try {
+      const product = await this.foodService.getProduct(barcode);
+      return product;
+    } catch (error) {
+      if (error.response?.status === 404) {
+        throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
+      } else {
+        throw new HttpException(
+          'Internal server error',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+  }
+}
--- src/food/food.service.ts
+++ src/food/food.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class FoodService {}
+export class FoodService {
+  private readonly API_URL =
+    'https://off:off@world.openfoodfacts.net/api/v2/product/';
+
+  async getProduct(barcode: string) {
+    const response = await axios.get(`${this.API_URL}${barcode}`);
+    return response.data;
+  }
+}
